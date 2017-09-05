0f2ec8a0fcd12c895d58c25b93ed3244516dc30b
Nicholas Halloran <nmhalloran@hotmail.com>
2017-09-05 20:46:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 var kittens = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendKitten() {
+  kittens.push()
+  return kittens
+}
+
+function destructivelyPrependKitten() {
+  kittens.unshift()
+  return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
