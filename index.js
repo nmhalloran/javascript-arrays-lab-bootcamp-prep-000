f44420c7ac11d266d726e152a89acc8343f9d7f8
Nicholas Halloran <nmhalloran@hotmail.com>
2017-09-05 21:11:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,3 +19,23 @@ function destructivelyRemoveFirstKitten() {
   kittens.shift()
   return kittens
 }
+
+function appendKitten(kitten) {
+  var newArray = kittens.push(kitten)
+  return newArray
+}
+
+function prependKitten(kitten) {
+  var newArray = kittens.unshift(kitten)
+  return newArray
+}
+
+function removeLastKitten() {
+  var newArray = kittens.pop()
+  return newArray
+}
+
+function removeFirstKitten() {
+  var newArray = kittens.slice(1)
+  return newArray
+}
